b997c734212499726dd3e71995bb6fad0deb59e0
taoalpha <iamzhoutao92@gmail.com>
2017-06-10 17:53:39 -0700

    add serialize, deserialize, def(), register()

--- index.js
+++ index.js
@@ -1,54 +1,74 @@
 /**
- * 
+ * Convert any function library into chainable library.
  */
 
 function Chainable(obj, options = {}) {
   if (!obj) throw new Error("Please provide a valid function library.");
   const storage = {};
-  storage._obj = obj;
-  storage._opts = options;
+  storage._obj = Object.create(obj);
+  storage._opts = Object.assign({
+    // default options
+  }, options);
 
+  // the instance returned by any function call in between
   function ChainInstance(store = function () { }, from) {
-    // return an instance has access to all methods in obj
+    // the store contains all information
+    //   - the function we called
+    //   - and the args we want to pass into
     return new Proxy(store, {
       apply: function (target, thisArg, args) {
         // return a ChainInstance with previous functions
         target.fns = target.fns || [];
         if (from === "get") {
-          target.fns[target.fns.length - 1].thisArg = thisArg;
           target.fns[target.fns.length - 1].args = args;
         }
         return new ChainInstance(target, "call");
       },
       get: function (target, name, receiver) {
-        // https://github.com/nodejs/node/issues/10731
-        // be careful about the initial symbol + string call when access the proxy(valueOf, util.inspect.custom, toStringTag)
         switch (name) {
           case "value":
-            return function (v) {
+            return function (...args) {
               // now start execute all fns
+              let v;
               for (let i = 0, len = target.fns.length; i < len; i++) {
                 let fn = target.fns[i];
-                let args = fn.args ? fn.args : [];
-                if (i > 0 || arguments.length !== 0) args.unshift(v);
-                v = fn.fn.apply(fn.thisArg, args);
+                let fnArgs = fn.args ? Array.from(fn.args) : [];
+                if (i === 0) {
+                  // merge args passed into value() with first fn's args
+                  fnArgs = args.concat(fnArgs);
+                } else {
+                  fnArgs.unshift(v);
+                }
+                v = fn.fn.apply(receiver, fnArgs);
               }
               return v;
-            }
+            };
+
+          // return the function
+          case "_origin":
+            return target.origin;
+
+          // retrieve all functions along the chain
           case "def":
             return function () {
-              return JSON.stringify(target.fns.map(fn => {
-                return { name: fn.fn.name, args: fn.args };
-              }));
+              target.fns.toString = function() {
+                return JSON.stringify(target.fns.map(fn => {
+                  return { name: fn.fn.name, args: fn.args };
+                }));
+              }
+              return target.fns;
             }
+
+          // serialize the fns
+          case "serialize":
+            // WARNING: serialize will lose all format information, be carefully when deal with format sensitive manipulation
+            return function() {
+              return target.fns.map(fn => `${fn.fn.name}${fn.args ? `(${fn.args.join(",")})` : ""}`).join(".");
+            };
+
           default:
-            // no support on symbol yet
-            // if (typeof name === "symbol") return;
-
-            // preserved words
-            // if (["inspect", "constructor", "prototype", "valueOf", "toString", "name"].indexOf(name) > -1) return;
-            if (!obj.hasOwnProperty(name)) return receiver;
-
+            let obj = storage._obj;
+            if (typeof obj[name] === "undefined") return receiver;
             let fn = typeof obj[name] === "function" ? obj[name] : function () { return obj[name]; };
             Object.defineProperty(fn, "name", { writable: true });
             fn.name = name;
@@ -57,6 +77,7 @@ function Chainable(obj, options = {}) {
             target.fns = target.fns || [];
             target.fns.push({ fn });
             target.fn = fn;
+            target.origin = obj[name];
             return new ChainInstance(target, "get");
         }
       }
@@ -65,24 +86,67 @@ function Chainable(obj, options = {}) {
 
   // every methods inside of the obj should also be accessible after convert to chainable one
   return new Proxy(storage, {
-    get: function (target, name) {
-      // https://github.com/nodejs/node/issues/10731
-      // be careful about the initial symbol + string call when access the proxy(valueOf, util.inspect.custom, toStringTag)
-      if (!obj.hasOwnProperty(name)) return new ChainInstance(target);
-
-      let fn = typeof obj[name] === "function" ? obj[name] : function () { return obj[name]; };
-      Object.defineProperty(fn, "name", { writable: true });
-      fn.name = name;
-
-      // return a ChainInstance with previous functions
+    get: function (target, name, receiver) {
+      let obj = storage._obj;
       let store = function () { };
-      store.fns = target.fns || [];
-      store.fns.push({ fn });
-      store.fn = fn;
+      store.fns = [];
+      switch (name) {
+        case "register":
+          // allow register new functions afterwards
+          return function (name, fn) {
+            if (typeof name === "function") {
+              fn = name;
+              name = fn.name;
+            }
+            if (!name) throw Chainable.CONSTANTS.errors.REGISTER_FUNCTION_NAME;
+            obj[name] = fn;
+          };
 
-      return new ChainInstance(store, "get");
+        case "def":
+          return function () {
+            return Chainable.CONSTANTS.errors.NO_VALID_METHOD_PROPERTY;
+          }
+        
+        case "deserialize":
+          return function (str) {
+            str.split(".").forEach(item => {
+              let [name, args] = item.split(/[()]/g);
+              args = args ? args.split(",") : undefined;
+              if (typeof obj[name] === "undefined") return;
+              let fn = typeof obj[name] === "function" ? obj[name] : function () { return obj[name]; };
+              Object.defineProperty(fn, "name", { writable: true });
+              fn.name = name;
+              if (args) store.fns.push({ fn, args });
+              else store.fns.push({ fn });
+              store.fn = fn;
+              store.origin = obj[name];
+            });
+            return new ChainInstance(store, "get");
+          };
+
+        default:
+          if (typeof obj[name] === "undefined") return receiver;
+          let fn = typeof obj[name] === "function" ? obj[name] : function () { return obj[name]; };
+          Object.defineProperty(fn, "name", { writable: true });
+          fn.name = name;
+
+          // return a ChainInstance with previous functions
+          store.fns.push({ fn });
+          store.fn = fn;
+          store.origin = obj[name];
+
+          return new ChainInstance(store, "get");
+      }
     }
   });
 }
+
+Chainable.CONSTANTS = {
+  errors: {
+    NO_VALID_METHOD_PROPERTY: "No valid method or property chained.",
+    REGISTER_FUNCTION_NAME: "Named function required."
+  }
+};
+
 
 module.exports = Chainable;
--- tests/main.js
+++ tests/main.js
@@ -5,17 +5,18 @@ const expect = require("expect.js"),
 
 describe("Chainable", function() {
 
-  let C1, C2;
+  let C1, C2, O1, O2;
 
   before(function () {
-    C1 = chainable({
+    O1 = {
       add (value, addend) { return value + (addend || 0); },
       sum () { return Array.prototype.slice.call(arguments).reduce((a, b) => a + b, 0); },
-      time (a, b) {return a * b;},
+      time (a = 1, b = 1) {return a * b;},
       print (value, prefix, suffix) { return (prefix || "") + value + (suffix || ""); }
-    });
-
-    C2 = chainable({
+    };
+    C1 = chainable(O1);
+    
+    O2 = {
       hello: "hi",
       toArray(...args) {
         return args;
@@ -27,15 +28,114 @@ describe("Chainable", function() {
       each(arr, cb) {
         return arr.map(cb);
       }
+    };
+    C2 = chainable(O2);
+  });
+
+  describe("test on interface on chainable functions", function () {
+    it("can access original methods", function () {
+      expect(C1.add._origin == O1.add).to.be(true);
+      expect(C1.sum._origin == O1.sum).to.be(true);
+      expect(C1.print._origin == O1.print).to.be(true);
+
+      expect(C2.toArray._origin == O2.toArray).to.be(true);
+    });
+
+    it("can access original properties", function() {
+      expect(C2.hello._origin == O2.hello).to.be(true);
+    });
+
+    it("has register function", function() {
+      expect(C2.register).to.be.a("function");
+    });
+
+    it("has def function", function() {
+      expect(C2.def).to.be.a("function");
+    });
+
+  });
+
+  describe("test on chain and evaluate it", function () {
+    it("can chain and call with no value", function () {
+      expect(C1.add(2).time(2).value()).to.be(4);
+      expect(C1.add(2).sum(1, 3, 4).value()).to.be(10);
+      expect(C2.toArray(2).push(2).push(4).each(v => v * 3).value()).to.eql([6, 6, 12]);
+    });
+
+    it("can chain and call with single value, honor the value passed in with value()", function () {
+      expect(C1.add.time(2).value(3)).to.be(6);
+      expect(C1.add(2).time(2).print().value(0)).to.be("4");
+      expect(C2.toArray(2).push(2).push(4).each(v => v * 3).value(1)).to.eql([3, 6, 6, 12]);
+    });
+
+    it("can chain and call with multiple values", function () {
+      expect(C1.sum.value(1, 2, 3)).to.be(6);
+      expect(C1.sum.add(2).time.time.time(2).value(1, 2, 3)).to.be(16);
+      expect(C2.toArray.value(1, 2, 3)).to.eql([1, 2, 3]);
     });
   });
 
-  it("Can chain it", function() {
-    expect(C1.add(2).time(2).value(0)).to.be(4);
-    expect(C1.add(2).sum(1, 3, 4).value(0)).to.be(10);
-    expect(C1.add(2).time(2).print().value(0)).to.be("4");
 
-    expect(C2.toArray(2).push(2).push(4).each(v => v * 3).value()).to.eql([6, 6, 12]);
+  describe("test on chain on invalid props", function () {
+    it("can chain and call with multiple invalid values", function () {
+      expect(C1.dd.ddddd.ddddd).to.be.ok;
+      expect(C1.dd.ddddd.ddddd.def()).to.be(chainable.CONSTANTS.errors.NO_VALID_METHOD_PROPERTY);
+    });
+
+    it("can chain and call with mixed invalid and valid values", function () {
+      expect(C1.dd.add.ddddd.def() + "").to.be(`[{"name":"add"}]`);
+    });
   });
 
+  describe("test on register", function () {
+    it("can register with valid name and function", function () {
+      C1.register("minus", function(value, minus) {
+        return value - minus;
+      });
+
+      expect(C1.add(2).minus(1).value(5)).to.be(6);
+    });
+
+    it("can register with valid function with name", function () {
+      C1.register(function divide(value, divideBy) {
+        return value / divideBy;
+      });
+
+      expect(C1.add(2).minus(1).divide(2).value(5)).to.be(3);
+    });
+
+    it("invalid register", function() {
+      expect(C1.register).withArgs(function(){}).to.throwException(chainable.CONSTANTS.errors.REGISTER_FUNCTION_NAME);
+    });
+  });
+
+  describe("def retrieve", function(){
+    it("can retrieve def with all valid methods", function() {
+      let def1 = C1.add(2, 3).time().time(2).def();
+      expect(def1 + "").to.be(`[{"name":"add","args":[2,3]},{"name":"time","args":[]},{"name":"time","args":[2]}]`);
+      expect(def1[0].fn === O1.add).to.be(true);
+      expect(def1[0].args).to.eql([2, 3]);
+
+      let def2 = C1.add.time(2).print.time.time.add.minus.divide.def();
+      expect(def2 + "").to.be(`[{"name":"add"},{"name":"time","args":[2]},{"name":"print"},{"name":"time"},{"name":"time"},{"name":"add"},{"name":"minus"},{"name":"divide"}]`);
+      expect(def2[4].fn === O1.time).to.be(true);
+    });
+
+    it("can retrieve def with invalid methods", function() {
+      let def = C1.add(2, 3).dd.ddd.dd.time(2).def();
+      expect(def.length).to.be(2);
+      expect(def + "").to.be(`[{"name":"add","args":[2,3]},{"name":"time","args":[2]}]`);
+    });
+  })
+
+  describe("serialize and deserialize", function(){
+    it("can serialize with valid methods", function() {
+      expect(C1.add(2, 3).dd.ddd.dd.time.time(2).serialize()).to.be(`add(2,3).time.time(2)`);
+    });
+
+    it("can serialize with invalid methods(ignore automatically)", function() {
+      let str = C1.minus(10, 3).dd.ddd.dd.time.time(2).serialize();
+      expect(C1.deserialize(str).value()).to.be(14);
+    });
+  })
 });
